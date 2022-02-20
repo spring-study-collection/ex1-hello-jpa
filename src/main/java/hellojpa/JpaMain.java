26273ce1671c0002d773ca063c580c87f471ad85
jungeun919 <renee8369@likelion.org>
2022-02-20 18:01:58 +0900

    테이블, 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,38 +19,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //객체를 생성한 상태(비영속)
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-            //객체를 저장한 상태(영속)
-//            em.persist(member1); //아직 DB에 저장된 것이 아님
-//            em.persist(member2);
+            em.persist(member);
 
-//            em.flush(); //강제로 flush 메커니즘 호출
-
-//            Member findMember1 = em.find(Member.class, 150L);
-//            Member findMember2 = em.find(Member.class, 150L);
-//            System.out.println("result = " + (findMember1 == findMember2));
-
-            //영속 엔티티 데이터 수정
-//            Member findMember = em.find(Member.class, 150L);
-//            findMember.setName("ZZZZZ");
-
-            //영속성 컨텍스트에서 분리(준영속)
-//            Member findMember = em.find(Member.class, 150L);
-//            findMember.setName("AAAAA");
-
-//            em.detach(findMember); //특정 엔티티만 준영속 상태로 전환
-//            em.clear(); //영속성 컨텍스트를 완전히 초기화
-//            em.close(); //영속성 컨텍스트를 종료
-
-//            Member findMember2 = em.find(Member.class, 150L);
-
-            //객체를 삭제한 상태(삭제)
-//            em.remove(member);
-
-            //커밋 후에 DB에 SQL 쿼리를 보냄
+            //DB에 SQL 쿼리를 보내고 커밋
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
