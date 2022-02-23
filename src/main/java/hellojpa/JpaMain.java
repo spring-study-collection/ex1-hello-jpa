05c9726d2a079aed8ff43543ea195d3c8dbc51de
jungeun919 <renee8369@likelion.org>
2022-02-23 17:10:43 +0900

    CASCADE와 고아 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,41 +20,21 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team teamB = new Team();
-            teamB.setName("teamB");
-            em.persist(teamB);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(teamB);
-            em.persist(member2);
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-            //2. 즉시로딩은 JPQL에서 N+1 문제를 일으킴
-//            List<Member> members = em.createQuery("select m from Member m", Member.class)
-//                    .getResultList();
-            //SQL: select * from Member
-            //SQL: select * from Team where TEAM_ID = xxx -> team, teamB 2번 실행
-
-            //1. LAZY를 사용해서 프록시로 조회
-//            Member m = em.find(Member.class, member1.getId());
-//
-//            System.out.println("m = " + m.getTeam().getClass()); //proxy
-//
-//            System.out.println("====================");
-//            m.getTeam().getName(); //프록시 객체 초기화, SQL 쿼리 실행
-//            System.out.println("====================");
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
+//            findParent.getChildList().remove(0);
 
             //DB에 SQL 쿼리를 보내고 커밋
             tx.commit();
